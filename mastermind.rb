2d54a741a47da219f7abde08caf20c849ab2b0d6
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-02 08:26:40 +0800

    finish the mastermind project

--- mastermind.rb
+++ mastermind.rb
@@ -1,21 +1,57 @@
 class Mastermind
-  attr_reader :answer
   def initialize
   	@answer = generate_answer
+  	@guesses = 12
+  	@win = false
+  end
+
+  def ask_to_guess
+  	while @guesses > 0 do
+  	  break if @win
+      print "#{@guesses} guesses left: "
+      input = gets.chomp.split("")
+      if guess_valid?(input)
+        win = guess(input)
+      else
+        puts "Invalid guess! Try again."
+      end
+    end
+    gameover
   end
 
   def guess(num)
-  	if num == answer
-  	  puts "Bingo! That's the answer!"
-  	  return true
+  	if num == @answer
+  	  @win = true
   	else
   	  a = get_A(num)
   	  b = get_B(num)
-  	  puts "Try again, #{a}A#{b}B.  "
-  	  return false
+  	  puts "Try again, #{a}A#{b}B."
+  	  @guesses -= 1
   	end
   end
 
+  def gameover
+  	input = nil
+  	until ["y", "n"].include?(input)
+  	  if @win
+        print "You win the game! Restart?[Y/N]"
+      else
+        print "You lose the game! Restart?[Y/N]"
+      end
+      input = gets.chomp.downcase
+    end
+    restart?(input)
+  end
+
+  def restart?(input)
+    case input
+    when "y"
+      true
+    when "n"
+      false
+    end
+  end
+  
   def generate_answer
   	answer = []
   	until answer.size == 4 do
@@ -23,6 +59,14 @@ class Mastermind
   	  answer << num unless answer.include?(num) 
   	end
   	answer
+  end
+
+  def guess_valid?(input)
+  	if input.size == 4 && input.all? { |e| "123456".include?(e) }
+  	  return true
+  	else
+  	  return false
+  	end
   end
 
   def get_A(num)
@@ -42,6 +86,16 @@ class Mastermind
   end
 end
 
-mastermind = Mastermind.new
-p mastermind.answer
-p mastermind.guess(["1", "2", "3", "4"])
+puts "***********************************************\n" \
+     "* Mastermind: can you guess the right number? *\n" \
+     "*   Guess a 4 digit number composed of 1-6.   *\n" \
+     "*   A means how many numbers are correct.     *\n" \
+     "*   B means how many numbers are misplaced.   *\n" \
+     "***********************************************"
+
+restart = true
+while restart do
+  mastermind = Mastermind.new
+  restart = mastermind.ask_to_guess
+end
+puts "Thanks for playing, byebye :)!"
